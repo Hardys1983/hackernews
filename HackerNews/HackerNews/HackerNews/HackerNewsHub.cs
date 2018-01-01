28b8b42ee5e8c985b3d06a823ce2248522824bbb
Hardys Perez Bermudez <hardys1983@gmail.com>
2018-01-01 12:48:43 -0500

    Move responsibilities from HackerNewsHub to NewsProcessor Add Urls to configuration files

--- HackerNews/HackerNews/HackerNews/HackerNewsHub.cs
+++ HackerNews/HackerNews/HackerNews/HackerNewsHub.cs
@@ -2,87 +2,46 @@
 using Microsoft.AspNet.SignalR.Hubs;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace HackerNews.HackerNews
 {
     public class HackerNewsHub: Hub
     {
-        private static NewsProcessor _newsProcessor = new NewsProcessor();
-        private static ConcurrentDictionary<int, HackerNewsModel> _news = new ConcurrentDictionary<int, HackerNewsModel>();
-        private static ConcurrentDictionary<string, string> _clientSearches = new ConcurrentDictionary<string, string>();
-        private static ConcurrentQueue<int> _idsOrder = new ConcurrentQueue<int>();
-
-        private static object _threadMutex = new Object();
-        private static Thread _thread; 
-
-        public HackerNewsHub()
+        public override Task OnConnected()
         {
-            if (_thread == null)
-            {
-                lock (_threadMutex)
-                {
-                    if (_thread == null)
-                    {
-                        _thread = new Thread(UpdateData);
-                        _thread.Start();
-                    }
-                }
-            }
+            NewsProcessor.Instance.DataChanged += RetrieveData;
+            NewsProcessor.Instance.ClientSearches.TryAdd(GetClientIdentifier(Context), null);
+
+            RetrieveData(null, null);
+            return base.OnConnected();
+        }
+        
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            NewsProcessor.Instance.ClientSearches.TryRemove(GetClientIdentifier(Context), out string RemovedSearchValue);
+            return base.OnDisconnected(stopCalled);
         }
 
-        private void UpdateData(object state)
+        public override Task OnReconnected()
         {
-            while (true)
-            {
-                var changed = false;
-                var ids = _newsProcessor.GetTopNews();
+            NewsProcessor.Instance.ClientSearches.TryGetValue(GetClientIdentifier(Context), out string SearchValue);
+            NewsProcessor.Instance.ClientSearches.AddOrUpdate(GetClientIdentifier(Context), SearchValue, (key, oldValue) => oldValue);
 
-                foreach (var id in ids)
-                {
-                    if (!_news.Keys.Contains(id))
-                    {
-                        _news.TryAdd(id, _newsProcessor.GetItem(id));
-                        changed = true;
-                    }
-                }
+            return base.OnReconnected();
+        }
 
-                foreach (var key in _news.Keys)
-                {
-                    if (!ids.Contains(key))
-                    {
-                        _news.TryRemove(key, out HackerNewsModel value);
-                    }
-                }
-
-                var positionsChanged = !_idsOrder.SequenceEqual(ids);
-                if (changed || positionsChanged)
-                {
-                    if (positionsChanged)
-                    {
-                        _idsOrder = new ConcurrentQueue<int>(ids);
-                    }
-
-                    RetrieveData();
-                }
-
-                Task.Delay(1000).Wait();
-            }
+        private static string GetClientIdentifier(HubCallerContext context)
+        {
+            return context.ConnectionId;
         }
 
         public void FilterBy(string filter)
         {
-            _clientSearches[GetClientIdentifier(Context)] = filter;
-            FilterResultForClient(_news.Values, GetClientIdentifier(Context), filter);
-        }
-
-        public static bool StringContains(string data, string needle)
-        {
-            return data.IndexOf(needle, System.StringComparison.InvariantCultureIgnoreCase) >= 0;
+            NewsProcessor.Instance.ClientSearches[GetClientIdentifier(Context)] = filter;
+            FilterResultForClient(NewsProcessor.Instance.News.Values, GetClientIdentifier(Context), filter);
         }
 
         private void FilterResultForClient(IEnumerable<HackerNewsModel> items, string connectionId, string filter)
@@ -94,45 +53,25 @@ namespace HackerNews.HackerNews
             Clients.Clients(new[] { connectionId }).updateTopHackerNews(JsonConvert.SerializeObject(result));
         }
 
-        public void RetrieveData()
+        public void RetrieveData(object sender, EventArgs e)
         {
-            var items = _news.Values.OrderByDescending(item => item.Score);
+            var items = NewsProcessor.Instance.News.Values.OrderByDescending(item => item.Score);
 
-            var withFilters = _clientSearches
+            var withFilters = NewsProcessor.Instance.ClientSearches
                                     .Where(pair => !string.IsNullOrEmpty(pair.Value))
                                     .Select(x => x.Key).ToArray();
 
             Clients.AllExcept(withFilters).updateTopHackerNews(JsonConvert.SerializeObject(items));
 
-            foreach (var searchConnection in _clientSearches.Where(pair => !string.IsNullOrEmpty(pair.Value)))
+            foreach (var searchConnection in NewsProcessor.Instance.ClientSearches.Where(pair => !string.IsNullOrEmpty(pair.Value)))
             {
                 FilterResultForClient(items, searchConnection.Key, searchConnection.Value);
             }
         }
 
-        public override Task OnConnected()
+        private static bool StringContains(string data, string needle)
         {
-            _clientSearches.TryAdd(GetClientIdentifier(Context), null);
-            return base.OnConnected();
-        }
-
-        public override Task OnDisconnected(bool stopCalled)
-        {
-            _clientSearches.TryRemove(GetClientIdentifier(Context), out string RemovedSearchValue);
-            return base.OnDisconnected(stopCalled);
-        }
-
-        public override Task OnReconnected()
-        {
-            _clientSearches.TryGetValue(GetClientIdentifier(Context), out string SearchValue);
-            _clientSearches.AddOrUpdate(GetClientIdentifier(Context), SearchValue, (key, oldValue) => oldValue);
-
-            return base.OnReconnected();
-        }
-
-        private static string GetClientIdentifier(HubCallerContext context)
-        {
-            return context.ConnectionId;
+            return data.IndexOf(needle, StringComparison.InvariantCultureIgnoreCase) >= 0;
         }
     }
 }
--- HackerNews/HackerNews/HackerNews/NewsProcessor.cs
+++ HackerNews/HackerNews/HackerNews/NewsProcessor.cs
@@ -1,16 +1,106 @@
 ﻿using Newtonsoft.Json;
 using RestSharp;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HackerNews.HackerNews
 {
     public class NewsProcessor
     {
-        private readonly RestClient _itemsIdsClient = new RestClient("https://hacker-news.firebaseio.com/v0/topstories.json");
-        private readonly RestClient _itemClient  = new RestClient("https://hacker-news.firebaseio.com/v0/item/");
+        public ConcurrentDictionary<int, HackerNewsModel> News { get; } = new ConcurrentDictionary<int, HackerNewsModel>();
+        public ConcurrentDictionary<string, string> ClientSearches { get; } = new ConcurrentDictionary<string, string>();
+        public ConcurrentQueue<int> IdsOrder { get; private set; } = new ConcurrentQueue<int>();
+        public event EventHandler DataChanged;
 
-        public IEnumerable<int> GetTopNews()
+        public static NewsProcessor Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_instanceMutex)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new NewsProcessor();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        private readonly RestClient _itemsIdsClient = new RestClient(ConfigurationManager.AppSettings["TopNewsUrl"]);
+        private readonly RestClient _itemClient  = new RestClient(ConfigurationManager.AppSettings["GetNewsItemUrl"]);
+        private Thread _thread;
+
+        private static object _instanceMutex = new Object();
+        private static NewsProcessor _instance;
+
+        private NewsProcessor()
+        {
+            _thread = new Thread(UpdateData);
+            _thread.Start();
+        }
+
+        private void UpdateData(object state)
+        {
+            while (true)
+            {
+                var changed = false;
+                var ids = GetTopNews();
+
+                if (!ids.Any())
+                {
+                    continue;
+                }
+
+                foreach (var id in ids)
+                {
+                    if (!News.Keys.Contains(id))
+                    {
+                        try
+                        {
+                            News.TryAdd(id, GetItem(id));
+                            changed = true;
+                        }
+                        catch
+                        {
+                            //TODO: Log Exception while retrieving item
+                        }
+                    }
+                }
+
+                foreach (var key in News.Keys)
+                {
+                    if (!ids.Contains(key))
+                    {
+                        News.TryRemove(key, out HackerNewsModel value);
+                    }
+                }
+
+                var positionsChanged = !IdsOrder.SequenceEqual(ids);
+                if (changed || positionsChanged)
+                {
+                    if (positionsChanged)
+                    {
+                        IdsOrder = new ConcurrentQueue<int>(ids);
+                    }
+
+                    DataChanged?.Invoke(null, null);
+                }
+
+                Task.Delay(1000).Wait();
+            }
+        }
+
+        private IEnumerable<int> GetTopNews()
         {
             var request = new RestRequest(Method.GET);
             request.AddUrlSegment("print", "pretty");
@@ -21,10 +111,11 @@ namespace HackerNews.HackerNews
                 return JsonConvert.DeserializeObject<IEnumerable<int>>(response.Content);
             }
 
-            throw new Exception("Could not retrieve the ids");
+            //Log issue
+            return Enumerable.Empty<int>();
         }
 
-        public HackerNewsModel GetItem(int id)
+        private HackerNewsModel GetItem(int id)
         {
             var request = new RestRequest($"{id}.json", Method.GET);
             request.AddUrlSegment("print", "pretty");
